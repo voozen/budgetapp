928d47ccb2a31569283ce55bfc18955e7e9818bd
voozen <voozen@gmail.com>
2025-05-26 12:09:41 +0200

    fix: backend tests fix

--- backend/src/test/java/com/example/pasir_oliwiak_bartosz/PASiROliwiakBartoszTests.java
+++ backend/src/test/java/com/example/pasir_oliwiak_bartosz/PASiROliwiakBartoszTests.java
@@ -2,8 +2,9 @@ package com.example.pasir_oliwiak_bartosz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.PasirOliwiakBartoszApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = PasirOliwiakBartoszApplication.class)
 class PASiROliwiakBartoszTests {
 
     @Test
--- backend/src/test/java/lib/lab12/pasir_oliwiak_bartosz/PasirOliwiakBartoszApplicationTests.java
+++ backend/src/test/java/lib/lab12/pasir_oliwiak_bartosz/PasirOliwiakBartoszApplicationTests.java
@@ -2,8 +2,9 @@ package lib.lab12.pasir_oliwiak_bartosz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.PasirOliwiakBartoszApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = PasirOliwiakBartoszApplication.class)
 class PasirOliwiakBartoszApplicationTests {
 
     @Test
